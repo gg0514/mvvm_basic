c1421362df2f2bb892052ba37fa748540d980be9
cmshouse <gg0514@gmail.com>
2024-04-26 22:33:58 +0900

    기본 파일 추가

---
+++ mvvm_basic/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace mvvm_basic
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ mvvm_basic/ViewModel/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mvvm_basic.ViewModel
+{
+    internal class MainViewModel
+    {
+    }
+}
