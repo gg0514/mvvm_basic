30b34a43c36aacfaf5f40a3b0ea29b407d1fd66d
cmshouse <gg0514@gmail.com>
2024-04-26 23:40:57 +0900

    View->VM->Model 바인딩 성공

--- mvvm_basic/Model/MainModel.cs
+++ mvvm_basic/Model/MainModel.cs
@@ -3,10 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace mvvm_basic.Model
 {
-    internal class MainModel
+    internal class MainModel : INotifyPropertyChanged
     {
+
+        private int num = 1;
+        private int num2 = 1;
+
+        public int Num
+        {
+            get { return num; }
+            set
+            {
+                num = value;
+                Num2 = value * 2;
+
+                OnPropertyChanged("Num");
+            }
+        }
+        public int Num2
+        {
+            get { return num2; }
+            set
+            {
+                num2 = value;
+
+                OnPropertyChanged("Num2");
+            }
+        }
+
+
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
     }
 }
--- mvvm_basic/ViewModel/MainViewModel.cs
+++ mvvm_basic/ViewModel/MainViewModel.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace mvvm_basic.ViewModel
 {
-    internal class MainViewModel
+    internal class MainViewModel : INotifyPropertyChanged
     {
+        private Model.MainModel model = null;
+
+        public Model.MainModel Model
+        {
+            get { 
+                return model; 
+            }
+            set { 
+                model = value; 
+                OnPropertyChanged("Model"); 
+            }
+        }
+
+
+        public MainViewModel()
+        {
+            model = new Model.MainModel();
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
     }
 }
